079834d8e9f302d19264bf5c39672f31da800796
Raffaele <r.apetino@studenti.unipi.it>
2017-10-06 00:55:04 +0200

    v0.1 partly works. I have to try the smtp server.

---
+++ checkcomm.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import smtplib
+import urllib.request
+
+
+while True:
+    
+	opener = urllib.request.FancyURLopener({})
+	url = "https://www.google.com"
+	f = opener.open(url)
+	content = f.read()
+	#uncomment the following line if you want the HTML code to be printed
+	#print(content) 
+	if str(content).find("Keyword or string") != -1: #if the keyword isn't found the code go to the else and send the email
+		time.sleep(60) #or it waits 60 seconds and download the HTML code again
+		continue
+	
+	else:
+		msg = 'Check the Website, now!'
+		fromaddr = 'your_email_add'
+		toaddrs  = ['fist_email_add','second_email_add', 'n_email_add']
+		#uncomment the following lines and set with your email smtp settings
+
+		#server = smtplib.SMTP('smtp.gmail.com', 587)
+		#server.starttls()
+		#server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
+		print('From: ' + fromaddr)
+		print('To: ' + str(toaddrs))
+		print('Message: ' + msg)
+		#server.sendmail(fromaddr, toaddrs, msg)
+		#server.quit()
+
+		break
